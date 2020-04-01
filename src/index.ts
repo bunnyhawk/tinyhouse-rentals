ed919556ca87aa2cd1ede3c44dc4e4c3c6e50080
Robert Higdon <robert@ulastnight.com>
2020-04-01 06:39:39 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+// import { schema } from './graphql'
+import { typeDefs, resolvers } from './graphql/index'
+
+const app = express();
+const port = 9000;
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' })
+
+// app.get('/listings', (_req, res) => {
+//   return res.send(listings);
+// });
+
+// app.post('/delete-listing', (req, res) => {
+//   const id: string = req.body.id;
+
+//   for (let i = 0; i < listings.length; i++) {
+//     if (listings[i].id === id) {
+//       return res.send(listings.splice(i, 1));
+//     }
+//   }
+
+//   return res.send('failed to delete listing');
+// });
+
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);
